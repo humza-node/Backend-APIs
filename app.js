fa2b9a594af4d3463269f2e84d89c9a5e1aeb489
Node <srs1@3rdeyesoft.com>
2023-12-19 15:40:57 +0500

    First Commit

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const bodyParser= require('body-parser');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const { v4: uuidv4 } = require('uuid'); 
+const { v4: uuidv4 } = require('uuid');
 const multer = require('multer');
 
 const mongoose = require('mongoose');
 
 const UserRoutes = require('./routes/User');
-
 const OtpRoutes = require('./routes/otp');
 const QuestionRouter = require('./routes/questions');
 const PaymentRoute = require('./routes/payment');
@@ -30,43 +29,36 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
-const storage = multer.diskStorage(
-  {
-    destination: function(req, file, cb)
-    {
-      cb(null, 'images');
-    },
-    filename: function(req, file, cb)
-    {
-      cb(null, uuidv4());
-    }
-  }
-);
+const storage = multer.memoryStorage(); // Use memory storage for binary data
 
-const filefilter = (req, file, cb) =>
-{
-  if(file.mimetype ==='image/png' || file.mimetype==='image/jpg' || file.mimetype==='image/jpeg')
-  {
+const filefilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
     cb(null, true);
-  }
-  else
-  {
+  } else {
     cb(null, false);
   }
 };
-app.use(multer({storage: storage, fileFilter: filefilter}).single('image'));
-app.use('/images', express.static(path.join(__dirname,'images')));
+
+app.use(multer({ storage: storage, fileFilter: filefilter }).single('image'));
+
+// No need to use express.static for 'images' in memory storage
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader(
-'Access-Control-Allow-Methods','OPTIONS, GET, POST, PUT, PATCH, DELETE'
-    );
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(bodyParser.json());
 app.use(UserRoutes);
@@ -86,10 +78,12 @@ app.use(ActiveRoute);
 app.use(workouts);
 app.use(trainRoute);
 app.use(ReviewRoute);
-mongoose.connect("mongodb+srv://admin:ltKn8qOm9drd5YJ2@students.vdzdpl9.mongodb.net/students?retryWrites=true&w=majority")
-.then(result =>
-    {
-        app.listen(3000);
-    })
-    .catch(err=>
- console.log(err));
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:ltKn8qOm9drd5YJ2@students.vdzdpl9.mongodb.net/students?retryWrites=true&w=majority'
+  )
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => console.log(err));
